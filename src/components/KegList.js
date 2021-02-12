dd034eb2c6d3a1d5796b8ec1eb86ece35bfabd28
Jeff Chiu <jwchiu82@gmail.com>
2021-02-12 11:28:07 -0800

    add keglist and newkegform code

---
+++ src/components/KegList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Keg from "./Keg";
+import PropTypes from 'prop-types';
+
+function KegList(props) {
+  return (
+    <React.Fragment>
+      {props.kegList.map((keg) => 
+        <Keg
+          whenKegClicked = {props.onKegSelection }
+          name = {keg.name}
+          brand = {keg.brand}
+          price = {keg.price}
+          alcoholContent = {keg.alcoholContent}
+          key={keg.id}
+          id={keg.id}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+KegList.propTypes = {
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func
+}
+
+export default KegList;
