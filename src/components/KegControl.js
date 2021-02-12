36e641a828c940eaf3fdcee9157bbe32d759175b
Jeff Chiu <jwchiu82@gmail.com>
2021-02-12 11:54:51 -0800

    add adding and editing of keg in kegcontrol

---
+++ src/components/KegControl.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import NewKegForm from './NewKegForm';
+import KegList from './KegList';
+import KegDetail from './KegDetail';
+import EditKegForm from './EditKegForm';
+
+class KegControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      masterKegList: [],
+      selectedKeg: null,
+      editing: false
+    };
+  }
+
+  handleAddingNewKegToList = (newKeg) => {
+    const newMasterKegList = this.state.masterKegList.concat(newKeg);
+    this.setState({
+      masterKegList: newMasterKegList,
+      formVisibleOnPage: false
+    });
+  }
+
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.masterKegList.filter(keg => keg.id ===id)[0];
+    this.setState({selectedKeg: selectedKeg});
+  }
+
+  handleEditKegInList = (kegToEdit) => {
+    const editedMasterKegList = this.state.masterKegList
+      .filter(keg => keg.id !== this.state.selectedKeg.id)
+      .concat(kegToEdit);
+    this.setState({
+      masterKegList: editedMasterKegList,
+      editing: false,
+      selectedKeg: null
+    });
+  }
+
+}
